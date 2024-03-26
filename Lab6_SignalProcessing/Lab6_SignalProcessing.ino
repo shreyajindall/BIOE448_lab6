ab0f17422279e4d9dce6f677c7b47c7e8184bdbe
shreyajindall <sj60@rice.edu>
2024-03-26 15:20:34 -0500

    Serial Monitor Prints Heartrate

--- Lab6_SignalProcessing/Lab6_SignalProcessing.ino
+++ Lab6_SignalProcessing/Lab6_SignalProcessing.ino
@@ -2,7 +2,15 @@ const int sensor_pin = 0;
 int pulse_signal = 0;
 float BPM = 0;
 
+bool any_peak_detected = false;
+bool first_peak_detected = false;
+unsigned long first_pulse_time = 0;
+unsigned long second_pulse_time = 0;
 
+int upper_threshold = 820;
+int lower_threshold = 810;
+
+int pulse_period = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,6 +20,28 @@ Serial.begin(9600);
 void loop() {
   // put your main code here, to run repeatedly:
 pulse_signal = analogRead(sensor_pin);
-Serial.println(pulse_signal);
-delay(20);
+//Serial.println(pulse_signal);
+
+
+if (pulse_signal > upper_threshold && any_peak_detected == false) {
+  any_peak_detected = true;
+  // Do something about this peak
+  if (first_peak_detected == false) {
+    first_pulse_time = millis();
+    first_peak_detected = true;
+
+  } else {
+    second_pulse_time = millis();
+    pulse_period = second_pulse_time - first_pulse_time;
+    BPM = 1000 * 60 / pulse_period;
+    Serial.println(BPM);
+    first_peak_detected = false;
+  }
+}
+
+if (pulse_signal < lower_threshold) {
+  any_peak_detected = false;
+}
+
+delay(50);
 }
