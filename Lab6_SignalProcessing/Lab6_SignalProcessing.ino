5c62dbde255d415feeb9c3cc62ac3d3b9766baef
shreyajindall <sj60@rice.edu>
2024-03-26 14:48:15 -0500

    View heartrate waveform

--- Lab6_SignalProcessing/Lab6_SignalProcessing.ino
+++ Lab6_SignalProcessing/Lab6_SignalProcessing.ino
@@ -1,9 +1,17 @@
+const int sensor_pin = 0;
+int pulse_signal = 0;
+float BPM = 0;
+
+
+
 void setup() {
   // put your setup code here, to run once:
-
+Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+pulse_signal = analogRead(sensor_pin);
+Serial.println(pulse_signal);
+delay(20);
 }
